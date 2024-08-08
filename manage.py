a33c7264307335533473a11db34d123ea0b0c6a2
shanshan-he <118197143+shanshan-he@users.noreply.github.com>
2024-08-08 17:29:38 +0800

    Add files via upload

---
+++ manage.py
@@ -0,0 +1,47 @@
+#! /usr/bin/env python
+# -*-coding:utf-8-*-
+from flask import Flask, render_template
+from flask import request
+import time
+
+from Speaker_Recognition import register, speakerrecog # 声纹识别库
+
+"""
+启动web程序
+
+"""
+
+app = Flask(__name__)
+
+# 首页面
+@app.route("/")
+def index():
+	return render_template("index.html")
+
+# 声纹注册
+@app.route("/speech",methods=['GET', 'POST'])
+def beginRecorder():
+	# printName = request.form["printNames"]
+	printName = request.form.get('printNames')
+	begin  = time.time()
+	register.train_model(printName)
+	# speechRecorder.run()
+	return "200"
+
+# 结束录音
+@app.route("/stopSpeech", methods=["GET", "POST"])
+def stopRecorder():
+	print("停止录音……")
+	speechRecorder.stop()
+	end = time.time()
+	return "200"
+
+# 说话人识别
+@app.route("/recognize", methods=['GET', 'POST'])
+def recognize():
+	return speakerrecog.speakerRecog()
+
+
+if __name__ == '__main__':
+	# 启动多线程参数，加快资源请求，快速响应用户
+	app.run(debug = True, host='localhost', port=8600, threaded = True)
---
+++ readme.md
@@ -0,0 +1,22 @@
+# 基于高斯混合模型的声纹识别系统
+
+由于要对声纹识别效果开展研究，优化了一下[该项目](https://github.com/lihanghang/CASR-DEMO?tab=readme-ov-file)，使其在Win10系统下能正常运行。
+
+## 启动方式
+
+```python
+pip install -r requirements.txt
+python manage.py
+```
+
+
+
+## 效果展示
+
+自动语音识别效果：
+
+![image-3](image/1.png)
+
+声纹识别效果：
+
+![image-3](image/2.png)
